664a743ba116c72f25afe98b3e5ad410b15febb1
jaffleman <jaffleman@gmail.com>
2026-07-20 20:13:32 +0200

    correction de la route de la base Géolockbdd

--- app/geoSeq.js
+++ app/geoSeq.js
@@ -1,7 +1,21 @@
 const { Sequelize } = require("sequelize");
 // console.log(process.env.PG_URL);
 // Ici on crée l'objet de type sequelize que tout mes models vont utiliser pour communiquer avec la BDD
-const geoSeq = new Sequelize(process.env.PG_URL2, {
+const {
+  DB_USER,
+  DB_PASSWORD,
+  DB_HOST,
+  DB_PORT,
+  DB_NAME,
+} = process.env;
+
+// ✅ ENCODAGE ICI
+const encodedPassword = encodeURIComponent(DB_PASSWORD);
+
+const connectionString = `postgres://${DB_USER}:${encodedPassword}@${DB_HOST}/geolockbdd`;
+
+console.log(connectionString); // debug
+const geoSeq = new Sequelize(connectionString, {
   // ici on peut rajouter des options
   // comme par exemple des elements que l'on veut ajouter à tout les models
   define: {
